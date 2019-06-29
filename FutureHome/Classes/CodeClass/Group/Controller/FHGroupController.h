8849c89b4d7bbf252c1bfceaac110e16e9fe7d49
同熙传媒 <tongxichuanmei@tongxichuanmeideMacBook-Pro.local>
2019-06-29 09:06:42 +0800

    Update

--- FutureHome/Classes/CodeClass/Group/Controller/FHGroupController.h
+++ FutureHome/Classes/CodeClass/Group/Controller/FHGroupController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FHGroupController : BaseViewController
+@interface FHGroupController : UIViewController
 
 @end
 
--- FutureHome/Classes/CodeClass/Group/Controller/FHGroupController.m
+++ FutureHome/Classes/CodeClass/Group/Controller/FHGroupController.m
@@ -8,7 +8,10 @@
 
 #import "FHGroupController.h"
 
-@interface FHGroupController ()
+@interface FHGroupController () <UIScrollViewDelegate>
+{
+    UIScrollView *mainScrollView;
+}
 
 @end
 
@@ -16,17 +19,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor purpleColor];
+    self.title = @"社群";
+    [self fh_setMainScrollView];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark — privite
+- (void)fh_setMainScrollView {
+    mainScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0,64, SCREEN_WIDTH, SCREEN_HEIGHT-64-SCREEN_HEIGHT/667 *50)];
+    mainScrollView.delegate = self;
+    mainScrollView.pagingEnabled = YES;
+    mainScrollView.showsHorizontalScrollIndicator = NO;
+    mainScrollView.showsVerticalScrollIndicator = NO;
+    mainScrollView.bounces =NO;
+//    [self.view insertSubview:mainScrollView belowSubview:navigationView];
+//    NSArray *views = @[self.dationVC.view, self.findingVC.view,self.rankingVC.view];
+//    self.viewControllers = @[self.dationVC,self.findingVC,self.rankingVC];
+//    for (int i = 0; i < self.viewControllers.count; i++){
+//        //添加背景，把三个VC的view贴到mainScrollView上面
+//        UIView *pageView = views[i];
+//        pageView.frame = CGRectMake(SCREEN_WIDTH * i, 0, mainScrollView.frame.size.width, mainScrollView.frame.size.height);
+//        [mainScrollView addSubview:pageView];
+//    }
+//    mainScrollView.contentSize = CGSizeMake(SCREEN_WIDTH * (self.viewControllers.count), 0);
 }
-*/
 
 @end
